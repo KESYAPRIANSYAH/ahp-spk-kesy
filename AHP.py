59f9a883d70ce85e69c90e1bd423855405502852
kesy apriansyah <kesyapriansyah@gmail.com>
2024-11-03 16:39:42 +0700

    Update AHP.py

--- AHP.py
+++ AHP.py
@@ -82,7 +82,7 @@ def calculate_ahp(A, B, n, m, criterias, alternatives):
 
 
 def main():
-    st.set_page_config(page_title="Kalkulator AHP", page_icon=":bar_chart:")
+    st.set_page_config(page_title="Kalkulator AHP Untuk Menentukan Jenis Gamifikasi Pop-Up Campaign", page_icon=":bar_chart:")
     st.header("Kalkulator AHP")
     st.sidebar.title("Kriteria & Alternatif")
 
