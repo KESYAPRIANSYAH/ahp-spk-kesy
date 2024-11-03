cd5454a5906e86519266407cdc20b49e3767d709
kesy apriansyah <kesyapriansyah@gmail.com>
2024-11-03 15:41:03 +0700

    Update AHP.py

--- AHP.py
+++ AHP.py
@@ -73,12 +73,14 @@ def calculate_ahp(A, B, n, m, criterias, alternatives):
     df_result['Ranking'] = df_result['Skor Akhir'].rank(ascending=False, method='first').astype(int)
     df_result = df_result.sort_values('Ranking')
 
-    st.write("### Hasil Akhir AHP:")
-    st.table(df_result[['Alternatif', 'Skor Akhir', 'Ranking']])
-
+    # Plot grafik hasil AHP
     st.pyplot(plot_graph(W2, criterias, "Kriteria", "Bobot Kriteria"))
     st.pyplot(plot_graph(W, alternatives, "Alternatif", "Alternatif Optimal untuk Kriteria yang Diberikan"))
     st.balloons()
+
+    # Menampilkan Hasil Akhir dengan Ranking di bagian paling bawah
+    st.write("### Hasil Akhir AHP:")
+    st.table(df_result[['Alternatif', 'Skor Akhir', 'Ranking']])
 
 
 def main():
