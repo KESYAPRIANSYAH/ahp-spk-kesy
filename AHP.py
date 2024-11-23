2ab3abce9ad6319fe0d2dc98ec85ddc4afd1abde
kesy apriansyah <kesyapriansyah@gmail.com>
2024-11-23 22:20:20 +0700

    Update AHP.py

--- AHP.py
+++ AHP.py
@@ -261,7 +261,7 @@ def main():
                 W = calculate_ahp(A, B, n, m, criterias, alternatives)
                 save_response(name, A, B, criterias, alternatives, W)
                 st.success("Data berhasil disimpan!")
-with tab2:
+    with tab2:
         st.subheader("Analisis Multi-Responden")
         
         if st.session_state.responses:
