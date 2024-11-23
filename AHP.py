a343f7c2a3be30afdd7f5eb2c67054e2ffd4d22a
kesy apriansyah <kesyapriansyah@gmail.com>
2024-11-23 21:27:52 +0700

    Update AHP.py

--- AHP.py
+++ AHP.py
@@ -148,8 +148,8 @@ def main():
     st.header("Kalkulator AHP Untuk Menentukan Jenis Gamifikasi Pop-Up Campaign")
     
     # Add tabs for input and analysis
-  
-    tab2 = st.tabs(["Input Data", "Analisis Responden"])
+    tab1 = st.tabs(["Input Data"])
+    tab2 = st.tabs(["Analisis Responden"])
     
     with tab1:
         # Respondent Information
